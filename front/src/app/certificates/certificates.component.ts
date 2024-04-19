c2056711a13bb7c66b2cf0f8e3d62c5708c28a76
danilo_c <danilo.cvijetic10@protonmail.ch>
2024-04-19 16:31:30 +0200

    feat: added certificates tree and mock certificates

--- front/src/app/certificates/certificates.component.ts
+++ front/src/app/certificates/certificates.component.ts
@@ -1,12 +1,76 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {SplitterModule} from 'primeng/splitter'
+import {TreeModule} from 'primeng/tree'
+import {TreeNode} from "primeng/api";
+import {FormatWidth, NgStyle} from "@angular/common";
+import {ButtonModule} from "primeng/button";
 
+export interface CertificateItem{
+  id: string,
+  name: string,
+  valid: boolean,
+  icon: string
+}
 @Component({
   selector: 'app-certificates',
   standalone: true,
-  imports: [],
+  imports: [
+    SplitterModule,
+    TreeModule,
+    NgStyle,
+    ButtonModule,
+  ],
   templateUrl: './certificates.component.html',
   styleUrl: './certificates.component.css'
 })
-export class CertificatesComponent {
 
+
+export class CertificatesComponent implements OnInit{
+  ngOnInit(): void {
+    this.certificates = [
+      {
+       key : "123",
+       label: "root cert",
+       checked: true,
+       icon: "icon.png",
+        children: [
+          {
+            key : "1223",
+            label: "ca cert",
+            checked: true,
+            icon: "icon.png",
+            children: [
+              {
+                key : "12239",
+                label: "end cert",
+                checked: true,
+                icon: "icon.png",
+              },
+
+              {
+                key : "23",
+                label: "end cert",
+                checked: true,
+                icon: "icon.png",
+
+              }
+            ]
+          }
+        ]
+      },
+    ]
+  }
+
+  certificates: TreeNode[] = []
+  selectedCertificate : TreeNode | null = null
+
+  protected readonly FormatWidth = FormatWidth;
+
+  addCertificate() {
+
+  }
+
+  removeCertificate() {
+
+  }
 }
---
+++ front/src/app/models/certificate.ts
@@ -0,0 +1,6 @@
+export interface Certificate {
+    serialNumber: bigint,
+    issuerName: string,
+    subjectName: string,
+    
+}
